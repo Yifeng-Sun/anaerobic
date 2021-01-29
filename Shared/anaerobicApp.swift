7242310e687939172e22f36d6ccf9f7cbc7137c3
sunyifeng <1549135178@qq.com>
2021-01-29 20:36:03 +0800

    Initial Commit

---
+++ Shared/anaerobicApp.swift
@@ -0,0 +1,17 @@
+//
+//  anaerobicApp.swift
+//  Shared
+//
+//  Created by 孙一峰 on 2021/1/29.
+//
+
+import SwiftUI
+
+@main
+struct anaerobicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
