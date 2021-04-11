8192f2e6ad2ee945eeafbb5a006cb13c71d3eeaa
sunyifeng <1549135178@qq.com>
2021-04-11 22:41:56 +0800

    before notification backup

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,50 +7,144 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        ZStack {
-            GradientCard().padding(.init(top: -100, leading: -100, bottom: 0, trailing: 0))
-            GradientCard()
-        }
-        
-        
+class Affair{
+    var name: String = ""
+    var description: String = ""
+    var reminderTime: Date = Date()
+    
+    init(name: String, description: String, reminderTime: Date) {
+        self.name = name
+        self.description = description
+        self.reminderTime = reminderTime
     }
+}
+var affair1 = Affair(name: "喂猫", description: "添一勺猫粮，加200毫升水。", reminderTime: Date())
+var affair2 = Affair(name: "iOS 课堂展示", description: "希望不要出 bug！", reminderTime: Date())
+var affair3 = Affair(name: "点个外卖", description: "食堂里的人太多了。", reminderTime: Date())
+
+var affairs: [Affair] = [affair1, affair2, affair3]
+
+struct ContentView: View {
+    @State var warm = true
+    @State var showpic = false
+    @State var cardnum = affairs.count
+    
+    var body: some View {
+        
+        
+        ScrollView(.horizontal) {
+            HStack(spacing: 20) {
+                ForEach(0..<cardnum, id: \.self) {currentIndex in
+                    GradientCard(affair: affairs[currentIndex])
+                }
+                Button(action:{
+                    withAnimation{
+                        print("添加事务按钮被触发")
+                        var newAffair = Affair(name: "未命名事务", description: "暂无描述", reminderTime: Date())
+                        affairs.append(newAffair)
+                        cardnum = affairs.count
+                    }
+                }) {
+                    Text("添加事务")
+                        .fontWeight(.bold)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .foregroundColor(.purple)
+                }
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                
+            }
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
 
 struct GradientCard: View {
+//    @Binding var warm: Bool
+//    @Binding var showpic: Bool
+    
     @State var warm = true
+    @State var showpic = false
+    @State var affair: Affair
+    
     var body: some View {
         VStack {
             VStack(alignment: .center, spacing: 4.0) {
-                Text("GradientCard")
+                Text(affair.name)
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .opacity(0.8)
                     .foregroundColor(.white)
-                Text(warm ? "From purple to red." : "From purple to blue.")
+                Text(affair.description)
                     .font(.body)
                     .opacity(0.4)
             }.frame(width: 300, height: 100)
+            if showpic{
+                VStack{
+                    Form {
+                        Section(header: Text("事务名称")) {
+                            TextField("Username", text: $affair.name)
+                        }
+                        Section(header: Text("事务描述")) {
+                            TextField("Username", text: $affair.description)
+                        }
+                        Section() {
+                            DatePicker(selection: $affair.reminderTime, label: { Text("时间") })
+                        }
+                        Section() {
+                            Button(action: {
+                                withAnimation{
+                                    showpic.toggle()
+                                    print("toggling 事务设置")
+                                    print(affair.reminderTime)
+                                }
+                            }) {
+                                Text("保存设置")
+                            }
+                        }
+                    }
+                    .navigationBarTitle("Settings")
+                    .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                    .projectionEffect(/*@START_MENU_TOKEN@*/ProjectionTransform(.identity)/*@END_MENU_TOKEN@*/)
+                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                }
+            }
             Button(action:{
                 withAnimation{
                     warm.toggle()
+                    //todo: zhucitongzhi
                     print("toggling skin")
                 }
             }) {
-                Text(warm ? "Change to cold" : "Change to warm")
+                Text(warm ? "开启通知" : "取消通知")
                     .fontWeight(.bold)
                     .padding()
                     .background(Color.white)
                     .cornerRadius(10)
                     .foregroundColor(.purple)
-            }.padding()
+            }
+            Text("事务设置")
+                .font(.system(size: 12, weight: .light))
+                .underline()
+                .opacity(0.4)
+                .onTapGesture {
+                    withAnimation{
+                        showpic.toggle()
+                        print("toggling skin")
+                    }
+                }
         }
         .padding()
         .background(warm ? LinearGradient(gradient: Gradient(
@@ -61,7 +155,7 @@ struct GradientCard: View {
                                         colors: [.purple, .blue]),
                                        startPoint: .topLeading,
                                        endPoint: .bottomTrailing))
-        .cornerRadius(20)
-        .opacity(warm ? 0.5 : 1)
+        .cornerRadius(30)
     }
 }
+
---
+++ Shared/page.swift
@@ -0,0 +1,133 @@
+//
+//  page.swift
+//  anaerobic
+//
+//  Created by 孙一峰 on 2021/4/11.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct PagingScrollView: View {
+    let items: [AnyView]
+
+    init<A: View>(activePageIndex:Binding<Int>, itemCount: Int, pageWidth:CGFloat, tileWidth:CGFloat, tilePadding: CGFloat, @ViewBuilder content: () -> A) {
+        let views = content()
+        self.items = [AnyView(views)]
+        
+        self._activePageIndex = activePageIndex
+        
+        self.pageWidth = pageWidth
+        self.tileWidth = tileWidth
+        self.tilePadding = tilePadding
+        self.tileRemain = (pageWidth-tileWidth-2*tilePadding)/2
+        self.itemCount = itemCount
+        self.contentWidth = (tileWidth+tilePadding)*CGFloat(self.itemCount)
+        
+        self.leadingOffset = tileRemain+tilePadding
+        self.stackOffset = contentWidth/2 - pageWidth/2 - tilePadding/2
+    }
+    
+    /// index of current page 0..N-1
+    @Binding var activePageIndex : Int
+    
+    /// pageWidth==frameWidth used to properly compute offsets
+    let pageWidth: CGFloat
+    
+    /// width of item / tile
+    let tileWidth : CGFloat
+    
+    /// padding between items
+    private let tilePadding : CGFloat
+    
+    /// how much of surrounding iems is still visible
+    private let tileRemain : CGFloat
+    
+    /// total width of conatiner
+    private let contentWidth : CGFloat
+    
+    /// offset to scroll on the first item
+    private let leadingOffset : CGFloat
+    
+    /// since the hstack is centered by default this offset actualy moves it entirely to the left
+    private let stackOffset : CGFloat // to fix center alignment
+    
+    /// number of items; I did not come with the soluion of extracting the right count in initializer
+    private let itemCount : Int
+    
+    /// some damping factor to reduce liveness
+    private let scrollDampingFactor: CGFloat = 0.66
+    
+    /// current offset of all items
+    @State var currentScrollOffset: CGFloat = 0
+    
+    /// drag offset during drag gesture
+    @State private var dragOffset : CGFloat = 0
+    
+    
+    func offsetForPageIndex(_ index: Int)->CGFloat {
+        let activePageOffset = CGFloat(index)*(tileWidth+tilePadding)
+        
+        return self.leadingOffset - activePageOffset
+    }
+    
+    func indexPageForOffset(_ offset : CGFloat) -> Int {
+        guard self.itemCount>0 else {
+            return 0
+        }
+        let offset = self.logicalScrollOffset(trueOffset: offset)
+        let floatIndex = (offset)/(tileWidth+tilePadding)
+        var computedIndex = Int(round(floatIndex))
+        computedIndex = max(computedIndex, 0)
+        return min(computedIndex, self.itemCount-1)
+    }
+    
+    /// current scroll offset applied on items
+    func computeCurrentScrollOffset()->CGFloat {
+        return self.offsetForPageIndex(self.activePageIndex) + self.dragOffset
+    }
+    
+    /// logical offset startin at 0 for the first item - this makes computing the page index easier
+    func logicalScrollOffset(trueOffset: CGFloat)->CGFloat {
+        return (trueOffset-leadingOffset) * -1.0
+    }
+    
+   
+    var body: some View {
+        GeometryReader { outerGeometry in
+            HStack(alignment: .center, spacing: self.tilePadding)  {
+                /// building items into HStack
+                ForEach(0..<self.items.count) { index in
+                    
+                        self.items[index]
+                            .offset(x: self.currentScrollOffset, y: 0)
+                            .frame(width: self.tileWidth)
+                    
+                }
+            }
+            .onAppear {
+                self.currentScrollOffset = self.offsetForPageIndex(self.activePageIndex)
+            }
+            .offset(x: self.stackOffset, y: 0)
+            .background(Color.black.opacity(0.00001)) // hack - this allows gesture recognizing even when background is transparent
+            .frame(width: self.contentWidth)
+            .simultaneousGesture( DragGesture(minimumDistance: 1, coordinateSpace: .local) // can be changed to simultaneous gesture to work with buttons
+                .onChanged { value in
+                    self.dragOffset = value.translation.width
+                    self.currentScrollOffset = self.computeCurrentScrollOffset()
+                }
+                .onEnded { value in
+                    // compute nearest index
+                    let velocityDiff = (value.predictedEndTranslation.width - self.dragOffset)*self.scrollDampingFactor
+                    let newPageIndex = self.indexPageForOffset(self.currentScrollOffset+velocityDiff)
+                    self.dragOffset = 0
+                    withAnimation(.interpolatingSpring(mass: 0.1, stiffness: 20, damping: 1.5, initialVelocity: 0)){
+                        self.activePageIndex = newPageIndex
+                        self.currentScrollOffset = self.computeCurrentScrollOffset()
+                    }
+                }
+            )
+        }
+    }
+}
